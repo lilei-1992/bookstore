f041421949f41990eec7df77da4c86d88923ad1d
lilei <314263806@qq.com>
2020-10-08 13:59:25 +0800

    test1

--- src/main/java/com/yd/config/ShiroConfig.java
+++ src/main/java/com/yd/config/ShiroConfig.java
@@ -1,86 +1,86 @@
-//package com.yd.config;
-//
-//
-//import com.yd.shiro.UserRealm;
-//import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
-//import org.apache.shiro.codec.Base64;
-//import org.apache.shiro.mgt.SecurityManager;
-//import org.apache.shiro.spring.LifecycleBeanPostProcessor;
-//import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
-//import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-//import org.apache.shiro.web.mgt.CookieRememberMeManager;
-//import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-//import org.apache.shiro.web.servlet.SimpleCookie;
-//import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.DependsOn;
-//
-//import javax.servlet.Filter;
-//import java.util.LinkedHashMap;
-//import java.util.Map;
-//
-//@Configuration
-//public class ShiroConfig {
-//    //将自己的验证方式加入容器
-//    @Bean
-//    public UserRealm userRealm() {
-//        UserRealm userRealm = new UserRealm();
-//        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
-//        //加密方式
-//        hashedCredentialsMatcher.setHashAlgorithmName("MD5");
-//        //加密次数
-//        hashedCredentialsMatcher.setHashIterations(11);
-//        //默认值，代表16机制值，如果设置false则为base64
-//        hashedCredentialsMatcher.setStoredCredentialsHexEncoded(true);
-//        userRealm.setCredentialsMatcher(hashedCredentialsMatcher);
-//        return userRealm;
+package com.yd.config;
+
+
+import com.yd.shiro.UserRealm;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.codec.Base64;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import javax.servlet.Filter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+    //将自己的验证方式加入容器
+    @Bean
+    public UserRealm userRealm() {
+        UserRealm userRealm = new UserRealm();
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+        //加密方式
+        hashedCredentialsMatcher.setHashAlgorithmName("MD5");
+        //加密次数
+        hashedCredentialsMatcher.setHashIterations(11);
+        //默认值，代表16机制值，如果设置false则为base64
+        hashedCredentialsMatcher.setStoredCredentialsHexEncoded(true);
+        userRealm.setCredentialsMatcher(hashedCredentialsMatcher);
+        return userRealm;
+    }
+
+    /**
+     * cookie对象;
+     *
+     * @return
+     */
+//    public SimpleCookie rememberMeCookie(){
+//        System.out.println("ShiroConfiguration rememberMeCookie--------------------------------------------");
+//        //这个参数是cookie的名称
+//        SimpleCookie simpleCookie = new SimpleCookie("rememberMe");
+//        //<!-- 记住我cookie生效时间30天 ,单位秒;-->
+//        simpleCookie.setMaxAge(2592000);
+//        return simpleCookie;
 //    }
 //
 //    /**
-//     * cookie对象;
-//     *
+//     * cookie管理对象;记住我功能
 //     * @return
 //     */
-////    public SimpleCookie rememberMeCookie(){
-////        System.out.println("ShiroConfiguration rememberMeCookie--------------------------------------------");
-////        //这个参数是cookie的名称
-////        SimpleCookie simpleCookie = new SimpleCookie("rememberMe");
-////        //<!-- 记住我cookie生效时间30天 ,单位秒;-->
-////        simpleCookie.setMaxAge(2592000);
-////        return simpleCookie;
-////    }
-////
-////    /**
-////     * cookie管理对象;记住我功能
-////     * @return
-////     */
-////    public CookieRememberMeManager rememberMeManager(){
-////        System.out.println("ShiroConfiguration rememberMeManager--------------------------------------------");
-////        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
-////        cookieRememberMeManager.setCookie(rememberMeCookie());
-////        //rememberMe cookie加密的密钥 建议每个项目都不一样 默认AES算法 密钥长度(128 256 512 位)
-////        cookieRememberMeManager.setCipherKey(Base64.decode("4KaYtkFJPs0KSA3Kprsdag=="));
-////        return cookieRememberMeManager;
-////    }
-//
-//    //权限管理，配置主要是Realm的管理认证
-//    @Bean
-//    public SecurityManager securityManager() {
-//        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-//        // 设置自定义验证规则
-//        securityManager.setRealm(userRealm());
-////        // 设置自定义rememeberMe
-////        securityManager.setRememberMeManager(rememberMeManager());
-//        return securityManager;
+//    public CookieRememberMeManager rememberMeManager(){
+//        System.out.println("ShiroConfiguration rememberMeManager--------------------------------------------");
+//        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+//        cookieRememberMeManager.setCookie(rememberMeCookie());
+//        //rememberMe cookie加密的密钥 建议每个项目都不一样 默认AES算法 密钥长度(128 256 512 位)
+//        cookieRememberMeManager.setCipherKey(Base64.decode("4KaYtkFJPs0KSA3Kprsdag=="));
+//        return cookieRememberMeManager;
 //    }
-//
-//    //Filter工厂，设置对应的过滤条件和跳转条件
-//    @Bean
-//    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
-//        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-//        shiroFilterFactoryBean.setSecurityManager(securityManager);
-//        return shiroFilterFactoryBean;
-//    }
-//
-//}
+
+    //权限管理，配置主要是Realm的管理认证
+    @Bean
+    public SecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        // 设置自定义验证规则
+        securityManager.setRealm(userRealm());
+//        // 设置自定义rememeberMe
+//        securityManager.setRememberMeManager(rememberMeManager());
+        return securityManager;
+    }
+
+    //Filter工厂，设置对应的过滤条件和跳转条件
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        return shiroFilterFactoryBean;
+    }
+
+}
