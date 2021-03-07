672a90a3ca5161fca71662814328ac8d3aa1c7dc
lilei <314263806@qq.com>
2021-03-07 13:05:08 +0800

    yuanceng

--- src/main/java/com/yd/controller/TestController.java
+++ src/main/java/com/yd/controller/TestController.java
@@ -2,7 +2,6 @@ package com.yd.controller;
 
 public class TestController {
     public static void main(String[] args) {
-        System.out.println("第一次提交master");
-        System.out.println("第一次提交test1");
+        System.out.println("第一次提交master1");
     }
 }
