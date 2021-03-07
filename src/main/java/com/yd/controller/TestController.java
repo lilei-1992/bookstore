4a8bffdfa38012ca2dc41aefe308b662fc573828
lilei <314263806@qq.com>
2021-03-07 12:42:43 +0800

    第一次提交

--- src/main/java/com/yd/controller/TestController.java
+++ src/main/java/com/yd/controller/TestController.java
@@ -2,8 +2,6 @@ package com.yd.controller;
 
 public class TestController {
     public static void main(String[] args) {
-        System.out.println(1111);
-        System.out.println("这是我的第二次提交dev2");
-        System.out.println("这是我的第二次提交dev2222");
+        System.out.println("第一次提交");
     }
 }
